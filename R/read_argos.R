9ec0d349f2a91bba193db71a9102413c0a27163b
Michael Sumner <mdsumner@gmail.com>
2015-06-15 15:58:52 +1000

    first pass at read_argos

---
+++ R/read_argos.R
@@ -0,0 +1,112 @@
+pushTimes <- function (time, id, pushincr = 1)
+{
+  dups <- unlist(tapply(time, id, duplicated), use.names = FALSE)
+  if (any(dups)) {
+    time[dups] <- time[dups] + pushincr
+    time <- Recall(time, id)
+  }
+  time
+}
+
+#' Read Argos PRV streams
+#'
+#' Read data from Argos PRV.
+#'
+#' See \code{\link{track_validity}} for details on the tests and fixes applied to records.
+#' @param x Connection to PRV records
+#' @param mungefix logical, should all track-validity errors be fixed.
+#' @param dtFormat
+#' @param tz
+#' @param duplicateTime_eps
+#' @param p4
+#' @param verbose
+#'
+#' @return A dplyr tbl_df
+#' @export
+#' @importFrom dplyr data_frame mutate distinct %>%
+read_argos <- function (x, correct.all = TRUE, dtFormat = "%Y-%m-%d %H:%M:%S",
+                        tz = "UTC", duplicateTime_eps = 0.01, p4 =  "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0",
+                        verbose = FALSE) {
+  dout <- vector("list", length(x))
+  for (icon in seq_along(x)) {
+    old.opt <- options(warn = -1)
+    dlines <- strsplit(readLines(x[icon]), "\\s+", perl = TRUE)
+    options(old.opt)
+    loclines <- sapply(dlines, length) == 12
+    if (any(loclines)) {
+      dfm <- matrix(unlist(dlines[sapply(dlines, length) ==
+                                    12]), ncol = 12, byrow = TRUE)
+      if (dfm[1, 7] == "LC") {
+        msg <- paste(" appears to be a diag file, skipping.",
+                     "Use read_diag to obtain a dataframe. \n\n")
+        cat("file ", icon, msg)
+        next
+      }
+      df <- vector("list", 12)
+      names(df) <- c("prognum", "ptt", "nlines", "nsensor",
+                     "satname", "class", "date", "time", "latitude",
+                     "longitude", "altitude", "transfreq")
+      for (i in c(1:4, 9:12)) df[[i]] <- as.numeric(dfm[, i])
+      for (i in 5:6) df[[i]] <- factor(dfm[, i])
+      for (i in 7:8) df[[i]] <- dfm[, i]
+      df <- as_data_frame(df) %>% mutate(utc = as.POSIXct(strptime(paste(df$date, df$time), dtFormat), tz))
+
+      dout[[icon]] <- df
+    }
+    else {
+      cat("Problem with file: ", x[icon], " skipping\n")
+    }
+  }
+  if (all(sapply(dout, is.null)))
+    stop("No data to return: check the files")
+  dout <- do.call(bind_rows, dout)
+  if (correct.all) {
+    ##dout <- dout[order(dout$ptt, dout$gmt), ]
+    ##dout <- dout[!duplicated(dout), ]
+    dout <- dout %>% arrange(ptt, utc) %>% distinct()
+    dt.by.id <- unlist(tapply(dout$utc, dout$ptt, function(x) c(-1,
+                                                                diff(x))))
+    dup.by.eps <- which(abs(dt.by.id) < duplicateTime_eps)
+    if (length(dup.by.eps) >= 1) {
+      if (verbose) {
+        cat("Adjusting duplicate times\n.....\n")
+        for (i in dup.by.eps) {
+          ind <- i + (-2:1)
+          print(cbind(dout[ind, c("ptt", "utc", "class")],
+                      row.number = ind))
+        }
+      }
+      dout$utc <- pushTimes(dout$utc, dout$ptt)
+      if (verbose) {
+        cat("\n  Adjusted records now: \n\n")
+        for (i in dup.by.eps) {
+          ind <- i + (-2:1)
+          print(cbind(dout[ind, c("ptt", "utc", "class")],
+                      row.number = ind))
+        }
+      }
+    }
+    if (any(dout$longitude > 180) & !grepl("+over", p4)) {
+      msg <- paste("\nLongitudes contain values greater than 180,",
+                   "assuming proj.4 +over\n\n")
+      cat(msg)
+      p4 <- sprintf("%s +over", p4)
+    }
+    dout$class <- ordered(dout$class, levels = c("Z", "B", "A", "0", "1", "2", "3"))
+    #     coordinates(dout) <- c("longitude", "latitude")
+    #     proj4string(dout) <- CRS(p4)
+    #     test <- try(dout <- trip(dout, c("gmt", "ptt")))
+    #     if (!is(test, "trip")) {
+    #       cat("\n\n\n Data not validated: returning object of class ",
+    #           class(dout), "\n")
+    #       return(dout)
+    #     }
+    #     cat("\n\n\n Data fully validated: returning object of class ",
+    #         class(dout), "\n")
+    #     return(dout)
+    #   }
+    #   cat("\n\n\n Data not validated: returning object of class ",
+    #       class(dout), "\n")
+  }
+    dout
+  }
